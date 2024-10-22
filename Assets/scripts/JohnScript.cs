a9ddf30ba9c9ed025f31e2a796096a237ccba9cf
NiekVrolijk <144125005+NiekVrolijk@users.noreply.github.com>
2024-10-22 15:39:34 +0200

    working on map

--- Assets/scripts/JohnScript.cs
+++ Assets/scripts/JohnScript.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class JohnScript : MonoBehaviour
 {
     public static int rations;
+    public static int johnLocation = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +16,6 @@ public class JohnScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Return)) { johnLocation++; }
     }
 }
--- Assets/scripts/mapScripts/pointScript.cs
+++ Assets/scripts/mapScripts/pointScript.cs
@@ -5,14 +5,44 @@ using UnityEngine;
 public class pointScript : MonoBehaviour
 {
     public int pointID = 0;
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    public Material unexploredMaterial;
+    public Material johnMaterial;
+    public Material exploredMaterial;
+
+    private bool johnHasBeen;
+
+    private Renderer rend;
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+        rend.sharedMaterial = unexploredMaterial;
+
+        johnHasBeen = false;
+    }
     void Update()
     {
+        TileStatus();
+    }
+
+    public void TileStatus()
+    {
         
+
+        if (pointID == JohnScript.johnLocation && rend.sharedMaterial != johnMaterial)
+        {
+            rend.sharedMaterial = johnMaterial;
+            johnHasBeen = true;
+        }
+        if (johnHasBeen && pointID != JohnScript.johnLocation && rend.sharedMaterial != exploredMaterial)
+        {
+            if (pointID != 0 && pointID != 9 && pointID != 19)
+            {
+                rend.sharedMaterial = exploredMaterial;
+            } else
+            {
+                rend.sharedMaterial = unexploredMaterial;
+            }
+        }
     }
 }
