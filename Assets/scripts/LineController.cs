8cd3f1082334c8841337d69c99ddcf1f3ee3c67d
NiekVrolijk <144125005+NiekVrolijk@users.noreply.github.com>
2024-10-22 13:32:35 +0200

    making map

--- Assets/scripts/LineController.cs
+++ Assets/scripts/LineController.cs
@@ -21,7 +21,6 @@ public class LineController : MonoBehaviour
         for (int i = 0; i < points.Length; i++)
         {
             lineRenderer.SetPosition(i, points[i].position);
-
         }
     }
 }
