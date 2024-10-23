df0033848e746456b09cea1aaabc7ce900cd23eb
Teun <teunrossiau@gmail.com>
2024-10-23 12:33:50 +0200

    card data display pt1

--- Assets/scripts/CardDisplay.cs
+++ Assets/scripts/CardDisplay.cs
@@ -3,15 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-
+using John_Project;
 public class CardDisplay : MonoBehaviour
 {
     public Card cardData;
     public Image cardImage;
+    public TMP_Text nameText;
+    public TMP_Text damageText;
+    public TMP_Text damageText2;
+    //public Image[] typeImages;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        UpdateCardDisplay();
+    }
+
+    public void UpdateCardDisplay()
+    {
+        nameText.text = cardData.cardName;
+        damageText.text = $"{cardData.damageMin} - {cardData.damageMax}";
+        damageText2.text = $"{cardData.damageMin} - {cardData.damageMax}";
+
     }
 
 }
--- Assets/scripts/HandManager.cs
+++ Assets/scripts/HandManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using John_Project;
 
 public class HandManager : MonoBehaviour
 {
