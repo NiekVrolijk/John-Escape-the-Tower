5eaee0971d977d3524778589b5293da4174425d8
NiekVrolijk <144125005+NiekVrolijk@users.noreply.github.com>
2024-10-23 14:25:46 +0200

    map tweaking

--- Assets/scripts/Encounter controller.cs
+++ Assets/scripts/Encounter controller.cs
@@ -6,10 +6,7 @@ using UnityEngine.UI;
 
 public class Encountercontroller : MonoBehaviour
 {
-    public pointScript pointScript;
-
     public GameObject popupPanel;
-    public GameObject textBox;
     public TextMeshProUGUI popupText;
     public Button closeButton;
 
@@ -18,11 +15,11 @@ public class Encountercontroller : MonoBehaviour
         "A trap triggers as you are engulfed in steaming hot cheese! You lose 100 Rations",
         "You encounter a treasure cheese filled with Gouda Cheese, a rare find indeed! You gain 100 rations.",
         "You fall asleep in stinky cheese, lose 25 rations.",
-        "You find a dead skeleton with 50 rations on him.",
-        "You find a dead cheese wolf with 75 rations.",
+        "You find a dead skeleton gain 50 rations.",
+        "You find a dead cheese wolf gain 75 rations.",
         "The room collapses and you lose 75 rations as you barely escape.",
         "A bag with molten cheese lies before you, you pick it up and you gain 50 rations.",
-        "The tyromancer has pickpocketed you and has stolen 100 rations!",
+        "The tyromancer has pickpocketed you, lose 100 Rations!",
     };
 
     private HashSet<int> visitedTiles = new HashSet<int>();
--- Assets/scripts/JohnScript.cs
+++ Assets/scripts/JohnScript.cs
@@ -16,6 +16,6 @@ public class JohnScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        Debug.Log(rations);
     }
 }
